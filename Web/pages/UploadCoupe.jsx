ba1e1658b0094e7b0702ed3f37dbf79acbe65634
GISFYFathima <fatima@gisfy.co.in>
2025-10-15 16:40:45 +0530

    Save local changes before pull

--- Web/pages/UploadCoupe.jsx
+++ Web/pages/UploadCoupe.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from "react";
 import { FaCloudUploadAlt } from "react-icons/fa";
+import { Link } from "react-router-dom"; 
 import "./UploadCoupe.css";
 import { useLanguage } from "../context/LanguageContext"; // Import language context
 
