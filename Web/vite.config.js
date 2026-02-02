de5eef175fa4c634bbe05f1e90eb70711a551cb5
Nikhil26082002 <nikhil@gisfy.co.in>
2026-02-02 17:42:59 +0530

    02-01-2026

--- Web/vite.config.js
+++ Web/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       // For your backend API (localhost:5002)
       "/api": {
-        target: "http://localhost:5002",
+        target: "http://68.178.167.216:5002",
         changeOrigin: true,
         secure: false,
       },
