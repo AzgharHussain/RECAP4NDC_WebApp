d30034d8321e91cb408a712fb8682aa801d713ab
Nikhil26082002 <nikhil@gisfy.co.in>
2026-02-06 11:03:28 +0530

    06-02-2026

--- api/index.js
+++ api/index.js
@@ -287,7 +287,7 @@ app.use((req, res, next) => {
 // ==================== START SERVER ==================== //
 const PORT = process.env.PORT || 5002;
 
-app.listen(PORT, async () => {
+app.listen(PORT, '0.0.0.0' , async () => {
   try {
     await sequelize.authenticate();
     console.log('🟢 Database connected successfully');
