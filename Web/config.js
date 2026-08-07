671b60a06b25747eab2191da4cc326cbd19ef67d
ven-katesh <venkatesh@gisfy.co.in>
2026-08-07 14:21:29 +0530

    07-08-2026

--- Web/config.js
+++ Web/config.js
@@ -1,4 +1,5 @@
- //export const API_BASE_URL = "https://forestrecap.gisfy.co.in";
-export const API_BASE_URL = "https://forestrecap.gisfy.co.in";
+// export const API_BASE_URL = "https://forestrecap.gisfy.co.in";
+export const API_BASE_URL = "http://localhost:5002";
 
-export const geoserverUrl = "https://gisfy.co.in:8445";
+// export const geoserverUrl = "https://gisfy.co.in:8445";
+export const geoserverUrl = "http://localhost:8080/geoserver";
